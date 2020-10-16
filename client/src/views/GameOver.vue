1d30ffac15b93b7f3263b23ff57c14dbd76fbf92
hutamafs <hutamafajarsaputra@gmail.com>
2020-10-16 09:36:00 +0700

    logic done

---
+++ client/src/views/GameOver.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>gameover</h1>
+        <button @click="play">Play again</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'GameOver',
+    methods:{
+        play() {
+            this.$store.commit('RESET')
+            this.$router.push({name:'Home'})
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
