9bc6cf3f00017bd81d766e3dc38e18ad606c3d26
rikyhidayat21 <rikyhidayat21@gmail.com>
2020-10-16 12:43:39 +0700

    edit home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,14 +3,16 @@
     <!-- start pertanyaan -->
     <div class="container-fluid p-0">
       <div class="item-flex mt-5" style="cursor: pointer;">
-        <h3 class="">Life: {{life}}</h3>
+        <h3 class="">Your Life: {{life}}</h3>
+        <h3>Your Skor: {{skor}}</h3>
       </div>
       <div class="item-flex text-center" style="cursor: pointer; letter-spacing: 2px">
         <b><H1>LETS PLAY</H1>
         <h3>RGB ({{clue.red}},{{clue.green}},{{clue.blue}})</h3>
         <p>CHOOSE THE RIGHT ONE</p></b></div>
       <div class="item-flex mt-5" style="cursor: pointer;">
-          <h3>Skor: {{skor}}</h3>
+        <h3>Enemy Life: {{eLife}} </h3>
+        <h3>Enemy Skor: {{eScore}} </h3>
       </div>
       <!-- <div class="flex bg-dark">
         <div class="text-center text-white">
@@ -29,9 +31,9 @@
     <div class="container">
       <h1 class="text-center">TIMES: 20</h1>
 
-      <p class="pl-1">LIVES: 3</p>
+      <!-- <p class="pl-1">LIVES: 3</p> -->
       <h2> {{time}} </h2>
-      <button @click="startGame">Start game</button>
+      <button class="btn btn-dark p-1 m-2" @click="startGame">Start game</button>
     </div>
     <Box
     v-for="color in colours"
